0ca56fe90bb52fa967c394bf9beb9a55ed88400d
cybermarkus1 <cybermarkus1@gmail.com>
2016-10-27 17:36:48 +0000

    gh2

---
+++ public/js/config.js
@@ -0,0 +1,52 @@
+require.config({
+  waitSeconds: 0,
+  baseUrl: 'public/js',
+  paths: {
+    // Libraries
+    jquery      : "../libs/vendors/jquery/jquery",
+    backbone    : '../libs/vendors/backbone-1.1.2/backbone',
+    handlebars  : "../libs/vendors/handlebars/handlebars",
+    underscore  : "../libs/vendors/underscore/underscore",
+    jade        : '../libs/vendors/jade/runtime',
+    bootstrap   : '../libs/vendors/bootstrap/bootstrap',
+    d3          : '../libs/vendors/d3/d3',
+    wizard         : '../backbone-wizard.min'
+
+  },
+
+  shim: {
+    jquery: {
+      exports: '$'
+    },
+    backbone:{
+      deps: ['jquery','underscore'],
+      exports: 'Backbone'
+    },
+    underscore: {
+      exports: '_'
+    },
+    handlebars: {
+      deps: ['backbone'],
+      exports: 'Handlebars'
+    },
+    bootstrap:{
+      deps: ['jquery']
+    },
+    wizard:{
+      deps:['underscore', 'backbone', 'd3']
+    }
+  }
+});
+
+define(function(require) {
+  'use strict';
+
+  var Backbone = require('backbone')
+  , AppRouter = require('apps/router')
+  ;
+
+  new AppRouter({});
+
+  Backbone.history.start();
+
+});
